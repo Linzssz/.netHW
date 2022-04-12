8a57f708cd5136d086745b5dc250adaba0b1ebdd
姿秀 林 <lss9999ch@yahoo.com.tw>
2022-04-12 17:28:07 +0800

    04/12

--- 2. FrmCategoryProducts.cs
+++ 2. FrmCategoryProducts.cs
@@ -7,19 +7,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace MyHomeWork
 {
+    
+    
+  //1.sqlconnetion
+ //2.sqlcommand
+ //3.sqldatareader
+ //4.ui control
     public partial class FrmCategoryProducts : Form
     {
         public FrmCategoryProducts()
         {
             InitializeComponent();
-        }
+            SqlConnection conn = null;
+            
+                conn = new SqlConnection("Data Source =.; Initial Catalog = Northwind; Integrated Security = True");
+                conn.Open();
+                SqlCommand command = new SqlCommand("select CategoryName From Categories", conn);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    this.comboBox1.Items.Add(reader["CategoryName"]);
+                }
+                conn.Close();
+            
+}
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-           // this.comboBox1.Text
+            SqlConnection conn = null;
+            string a = (comboBox1.SelectedIndex + 1).ToString();
+            conn=new SqlConnection("Data Source =.; Initial Catalog = Northwind; Integrated Security = True");
+            conn.Open();
+            SqlCommand command = new SqlCommand("Select*from Products where CategoryID="+a,conn);
+            SqlDataReader reader = command.ExecuteReader();
+            listBox1.Items.Clear();
+            while(reader.Read())
+            {
+                this.listBox1.Items.Add(reader["ProductName"]);
+            }
+            conn.Close();
         }
     }
 }
---
+++ 3. FrmProducts.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyHW
+{
+    public partial class FrmProducts : Form
+    {
+        public FrmProducts()
+        {
+            InitializeComponent();
+            this.productsTableAdapter1.Fill(nwDataSet1.Products);
+            this.bindingSource1.DataSource = nwDataSet1.Products;
+            this.dataGridView1.DataSource = bindingSource1;
+            this.bindingNavigator1.BindingSource  = bindingSource1;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            this.bindingSource1.MovePrevious();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.bindingSource1.MoveFirst();
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            this.bindingSource1.MoveNext();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            this.bindingSource1.MoveLast();
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+            label2.Text = $"{this.bindingSource1.Position + 1} / {this.bindingSource1.Count}";
+            lblResult.Text = $"共{this.bindingSource1.Count}則結果";
+        }
+
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int A = int.Parse(textBox1.Text);
+            int B = int.Parse(textBox2.Text);
+            this.productsTableAdapter1.UnitPriceFillBy(this.nwDataSet1.Products, A, B);
+            this.bindingSource1.DataSource = nwDataSet1.Products;
+            this.dataGridView1.DataSource = bindingSource1;
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String A = textBox3.Text;
+            this.productsTableAdapter1.FillByPN (this.nwDataSet1.Products,A);
+            this.bindingSource1.DataSource = nwDataSet1.Products;
+            this.dataGridView1.DataSource = bindingSource1;
+        }
+    }
+}
